ef52098abefe8f128a654cf4bbcecb2f5d20163b
Ivo Valchev <ivo.v.valchev@gmail.com>
2020-09-25 16:16:07 +0200

    Add GA

--- input/script.js
+++ input/script.js
@@ -29,7 +29,6 @@ function handleModeChange(isDark) {
 }
 /** END DARK MODE TOGGLE **/
 
-
 /**
  * HAMBURGER
  **/
@@ -48,8 +47,21 @@ function onMenuClick() {
 /**
   * GALLERY
   **/
-
 window.addEventListener('load', function() {
   const result = baguetteBox.run('.gallery');
 });
 /** END GALLERY **/
+
+
+/**
+  * GOOGLE ANALYTICS
+  **/
+if(true) {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+}
+if (typeof ga === "function") {
+  ga('create', 'UA-114536758-1', 'auto', {});
+/** END GOOGLE ANALYTICS **/
